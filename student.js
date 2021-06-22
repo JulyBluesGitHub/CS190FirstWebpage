174a82eabc5a3cdd3170738d4190427b9bdc852f
JulyBluesGitHub <76235568+JulyBluesGitHub@users.noreply.github.com>
2021-06-21 21:36:35 -0700

    Update student.js

--- student.js
+++ student.js
@@ -1,1 +1,12 @@
+function confirmlink(linkname) {
+   if (confirm("Are you sure you want to jump to " + linkname + "?"))
+        location = linkname;
+}
+
+function displayinfo() {
+  alert("my name is " + myname + " the class is an " + classdescription + " the date is currently " + thedate);
+}
+myname = "Jose";
+classdescription ="introduction to Javascript";
+thedate = "06/2021";
 
